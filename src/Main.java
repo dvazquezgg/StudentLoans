5a1db72969965089eac8fbf7e178ef9b40fff3c7
Daniel Vazquez <dvazquez@greengates.edu.mx>
2022-12-12 09:49:30 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Student Loan System");
+
+        Student student = new Student(93003, "Smith");
+
+        System.out.println("Student ID: " + student.getStudentID());
+
+    }
+}
